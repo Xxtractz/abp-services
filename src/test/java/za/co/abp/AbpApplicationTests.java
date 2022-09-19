11dd1e21b9bc0badf127327a60904a645d1f12e5
musambaloyi@gmail.com <@FAcebook1>
2022-09-19 23:31:54 +0200

    #comment init commit

---
+++ src/test/java/za/co/abp/AbpApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.abp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
